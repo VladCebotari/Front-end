1a5f53ce0989a36947cbcc6dc5ff4d489aaeba85
DenisaBoloh <bolohandenisa1@gmail.com>
2024-03-25 16:26:55 +0200

    Actualizare imagine de fundal

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Plate
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+import React from 'react';
+import './styles.css'; 
+import backgroundImage from './styles.css'; 
+
 
 function App() {
   return (
-    <div className="App">
+    <div className="background-image" style={{ backgroundImage: `url(${backgroundImage})`, display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <p style={{ fontSize: '30px', color: 'black', fontWeight: 'bold' }} >
+          Welcome to
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+        <a style={{ fontSize: '20px', color: 'black' }}>
+              PlateSnap
         </a>
       </header>
     </div>
   );
 }
 
+
+
 export default App;
+
