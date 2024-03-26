e52f01c1931a38376b608744bf57a69b4613624e
DenisaBoloh <bolohandenisa1@gmail.com>
2024-03-26 16:28:06 +0200

    welcome page

--- src/App.js
+++ src/App.js
@@ -27,17 +27,19 @@
 import React from 'react';
 import './styles.css'; 
 import backgroundImage from './styles.css'; 
+import logoImage from './logo page.png'
 
 
 function App() {
   return (
-    <div className="background-image" style={{ backgroundImage: `url(${backgroundImage})`, display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
-      <header className="App-header">
-        <p style={{ fontSize: '30px', color: 'black', fontWeight: 'bold' }} >
+    <div className="background-image" style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+      <header className="App-header" style={{ textAlign: 'center' }}>
+        <p style={{ fontSize: '50px', color: 'black', fontWeight: 'bold', fontFamily: 'Consolas' }}>
           Welcome to
         </p>
-        <a style={{ fontSize: '20px', color: 'black' }}>
-              PlateSnap
+        <img src={logoImage} alt="Logo" style={{ width: '300px', height: 'auto', filter: 'invert(100%)', marginBottom: '30px' }} />
+        <a style={{ fontSize: '20px', color: 'black', display: 'inline-block', textDecoration: 'none' }}>
+          Click to start
         </a>
       </header>
     </div>
@@ -46,5 +48,6 @@ function App() {
 
 
 
-export default App;
 
+
+export default App;
